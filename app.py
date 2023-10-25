154001824013c95773c016f7eeeb9be76d973589
Bhagyashree Mahajan <118426557+shreearn@users.noreply.github.com>
2023-10-25 18:39:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "CV.pdf"
-profile_pic = current_dir / "assets" / "profile_pic.jpg"
+profile_pic = current_dir / "assets" / "profile pic.jpg"
 
 # Genral settings
 PAGE_TITLE = "Digital CV | Bhagyashree Mahajan"
